6e9ab842795b14a5b3a96118948717a0552b7792
yunbae <yoonbea12345@gmail.com>
2026-02-19 16:07:05 +0900

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+/* 
+ * Design Philosophy: Neo-Brutalism Digital Street Culture
+ * Dark mode with electric cyan (#00F0FF) and hot magenta (#FF006E) accents
+ */
+import MVP from "./pages/mvp";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+
+
+function Router() {
+  return (
+    <Switch>
+  <Route path={"/mvp"} component={MVP} />
+  <Route path={"/"} exact component={Home} />
+  <Route path={"/404"} component={NotFound} />
+  <Route component={NotFound} />
+</Switch>
+
+  );
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="dark">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
